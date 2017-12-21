d14523d11a5404cff97f71faf27371f671009cd0
Jakub Matejka <jakub@keboola.com>
2017-12-21 17:13:13 +0100

    :cop: Fix deploy - travis debug

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm \
     -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
     quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP}`
+echo "REPOSITORY: $REPOSITORY"
 docker tag ${KBC_APP_REPOSITORY}:latest ${REPOSITORY}:${TRAVIS_TAG}
 docker tag ${KBC_APP_REPOSITORY}:latest ${REPOSITORY}:latest
 eval $(docker run --rm \
